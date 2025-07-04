55b68cfb53f9640219b6348761f5bcc3d9f5d72a
unknown <stevenoscar458@gmail.com>
2025-07-04 14:56:19 +0700

    Add google maps

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -15,6 +15,9 @@ import 'package:catering_app/widgets/text_form_field_widget.dart';
 import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DashboardScreen extends StatefulWidget {
   static String id = "/dashboard";
@@ -27,6 +30,10 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   late Future<ResponseModel<List<MenuModel>>> menuFuture;
   late User userData;
+  GoogleMapController? mapController;
+  LatLng _currentPosition = LatLng(-6.200000, 106.816666);
+  String _currentAddress = "Alamat tidak ditemukan";
+  Marker? _marker;
   TextEditingController searchController = TextEditingController();
   final FToast fToast = FToast();
   DateTime today = DateTime.now();
@@ -37,7 +44,45 @@ class _DashboardScreenState extends State<DashboardScreen> {
     super.initState();
     fToast.init(context);
     _initializeUser();
+    _getCurrentLocation();
     menuFuture = CateringApi.getMenu();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.openLocationSettings();
+      return;
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse && permission != LocationPermission.always) {
+        return;
+      }
+    }
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    _currentPosition = LatLng(position.latitude, position.longitude);
+
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      _currentPosition.latitude,
+      _currentPosition.longitude,
+    );
+    Placemark place = placemarks[0];
+
+    setState(() {
+      _marker = Marker(
+        markerId: MarkerId("lokasi_saya"),
+        position: _currentPosition,
+        infoWindow: InfoWindow(title: "Lokasi anda", snippet: "${place.street}, ${place.locality}"),
+      );
+    });
+
+    _currentAddress = "${place.name}, ${place.street}, ${place.locality}, ${place.country}";
+
+    mapController?.animateCamera(
+      CameraUpdate.newCameraPosition(CameraPosition(target: _currentPosition, zoom: 16)),
+    );
   }
 
   Future<void> _initializeUser() async {
@@ -326,16 +371,63 @@ class _DashboardScreenState extends State<DashboardScreen> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
-                          children: [
-                            Icon(Icons.location_on, size: 28, color: AppColor.mainLightGreen),
-                            const SizedBox(width: 4),
-                            Text(
-                              "PPKD Jakarta Pusat",
-                              style: AppTextStyles.body2(fontWeight: FontWeight.w800),
-                            ),
-                            const Icon(Icons.keyboard_arrow_down_rounded, size: 28),
-                          ],
+                        GestureDetector(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder:
+                                  (context) => AlertDialog(
+                                    content: SizedBox(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Stack(
+                                        children: [
+                                          GoogleMap(
+                                            initialCameraPosition: CameraPosition(
+                                              target: _currentPosition,
+                                              zoom: 14,
+                                            ),
+                                            onMapCreated: (controller) {
+                                              mapController = controller;
+                                            },
+                                            markers: _marker != null ? {_marker!} : {},
+                                            myLocationButtonEnabled: true,
+                                            myLocationEnabled: true,
+                                          ),
+                                          Positioned(
+                                            top: 16,
+                                            left: 16,
+                                            right: 16,
+                                            child: Card(
+                                              color: AppColor.white,
+                                              elevation: 4,
+                                              child: Padding(
+                                                padding: EdgeInsets.all(12),
+                                                child: Text(
+                                                  _currentAddress,
+                                                  style: AppTextStyles.body3(
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                            );
+                          },
+                          child: Row(
+                            children: [
+                              Icon(Icons.location_on, size: 28, color: AppColor.mainLightGreen),
+                              const SizedBox(width: 4),
+                              Text(
+                                "PPKD Jakarta Pusat",
+                                style: AppTextStyles.body2(fontWeight: FontWeight.w800),
+                              ),
+                              const Icon(Icons.keyboard_arrow_down_rounded, size: 28),
+                            ],
+                          ),
                         ),
                         GestureDetector(
                           onTap: () {
